916ccd0a6e4fa515b3dbcf6f4ded28bbd6aa97b5
Gustav Kutugutov <gustav.kutugutov@gmail.com>
2021-07-23 16:19:01 +0900

    init

---
+++ api/controllers/SiteController.php
@@ -0,0 +1,49 @@
+<?php
+namespace api\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\filters\AccessControl;
+
+/**
+ * Site controller
+ */
+class SiteController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return '{}';
+    }
+
+}
