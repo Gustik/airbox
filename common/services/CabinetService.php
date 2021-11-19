905cbc5763be7574718c1a0fe66f31dee681667b
Gustav Kutugutov <gustav.kutugutov@gmail.com>
2021-11-19 12:35:09 +0900

    json support for docker images

--- common/services/CabinetService.php
+++ common/services/CabinetService.php
@@ -8,6 +8,9 @@ class CabinetService implements ICabinetService
 {
     static function openCell(string $cellAddress): bool
     {
+        /*$res = file_get_contents('http://modbus/');
+        $cell = json_decode($res);
+        return ($cell->OPENED == 1);*/
         return true;
     }
 
